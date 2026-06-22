369937b8ec34aa82bb2665408a3487d93f2fff7e
marufbytes <marufahammed215@gmail.com>
2026-06-23 02:31:13 +0600

    Nest js

--- Practice/practice.js.js
+++ Practice/practice.js.js
@@ -1,6 +1,35 @@
-const spArr = [10,20,30,40];
+let sti1 = true;
+function getUserList() {
+    return new Promise((resolve, reject) => {
+        if (sti1) {
+            setTimeout(() => {
+                resolve("user 1")
+            }, 2000);
+        }
 
-function spreadTest(a,b,c,d){
-    return a+b+c+d;
+        else {
+            reject("something went wrong");
+        }
+    });
 }
-console.log(spreadTest(...spArr))
+let sti2 = false;
+function greetUser() {
+    return new Promise((resolve, reject) => {
+        if (sti2) {
+            setTimeout(() => {
+                resolve(
+                    " welcome"
+                );
+
+            }, 3000)
+        }
+        else {
+            reject("greet user went wrong")
+        }
+    });
+}
+
+const p = greetUser();
+p.then((msg)=>{
+    console
+})
