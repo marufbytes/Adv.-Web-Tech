86b27bffc35a6ead22bc5d9fc45f57f3e01ffb1a
marufbytes <marufahammed215@gmail.com>
2026-07-07 12:40:51 +0600

    Lab task 1 resubmit

---
+++ Lab_Task_01/src/course/course.controller.ts
@@ -0,0 +1,37 @@
+import { Controller,  Delete, Get, Param, Patch, Post, Put  } from '@nestjs/common';
+import { CourseService } from './course.service';
+
+@Controller('course')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get()
+    getAllCources(){
+        return this.courseService.getAllCourses();
+    }
+
+    @Get(':id')
+    getCourseByID(@Param('id') id:string){
+        return this.courseService.getCourseByID(id);
+    }
+
+    @Post()
+    createCourse(){
+        return this.courseService.creatCourse();
+    }
+
+    @Put(':id')
+    updateCourse(@Param('id')id:string){
+        return this.courseService.updateCourse(id);
+    }
+
+    @Patch(':id')
+    patchCourse(@Param('id')id:string){
+        return this.courseService.patchCouse(id);
+    }
+
+    @Delete(':id')
+    deleteCourse(@Param('id')id:string){
+        return this.courseService.deleteCourse(id);
+    }
+}
